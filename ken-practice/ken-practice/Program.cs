b8efa2f60c26f5d3ef44cb6eb3a99c7ebf6962cd
Jada <jada.fagan@gmail.com>
2020-06-08 19:43:13 -0400

    changed hello world to hello jada

--- ken-practice/ken-practice/Program.cs
+++ ken-practice/ken-practice/Program.cs
@@ -6,7 +6,7 @@ namespace ken_practice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Jada!");
         }
     }
 }
